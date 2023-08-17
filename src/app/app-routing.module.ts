57218eb11ad54fb6420807030de03ae55044be8d
tharanga270 <developerwordpress270@gmail.com>
2023-08-17 15:29:48 +0530

    route-1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './core/not-found/not-found.component';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) }, { path: 'about', loadChildren: () => import('./modules/about/about.module').then(m => m.AboutModule) }, { path: 'blog', loadChildren: () => import('./modules/blog/blog.module').then(m => m.BlogModule) }, { path: 'singleblog', loadChildren: () => import('./modules/singleblog/singleblog.module').then(m => m.SingleblogModule) }, { path: 'departments', loadChildren: () => import('./modules/departments/departments.module').then(m => m.DepartmentsModule) }, { path: 'elements', loadChildren: () => import('./modules/elements/elements.module').then(m => m.ElementsModule) }, { path: 'contact', loadChildren: () => import('./modules/contact/contact.module').then(m => m.ContactModule) }];
+const routes: Routes = [
+  {path:'', redirectTo:'home/index', pathMatch:'full'},
+  { path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => 
+    m.HomeModule) }, 
+  { path: 'about', loadChildren: () => import('./modules/about/about.module').then(m =>
+     m.AboutModule) }, 
+  { path: 'blog', loadChildren: () => import('./modules/blog/blog.module').then(m => 
+    m.BlogModule) }, 
+  { path: 'singleblog', loadChildren: () => import('./modules/singleblog/singleblog.module').then(m =>
+     m.SingleblogModule) }, 
+  { path: 'departments', loadChildren: () => import('./modules/departments/departments.module').then(m => 
+    m.DepartmentsModule) }, 
+  { path: 'elements', loadChildren: () => import('./modules/elements/elements.module').then(m =>
+     m.ElementsModule) }, 
+  { path: 'contact', loadChildren: () => import('./modules/contact/contact.module').then(m =>
+     m.ContactModule) },
+  { path:'**', component:NotFoundComponent}
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { HeaderComponent } from 'src/app/core/header/header.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  // { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent, children:[
+    {path:'', redirectTo:'index', pathMatch:'full'},
+    {path:'index',component:HeaderComponent},
+  ] }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
